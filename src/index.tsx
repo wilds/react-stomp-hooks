ed0fe27e515db3b88e9b40e180e1c3ccdbfe84f6
Wilds <ivoselva@gmail.com>
2021-10-12 12:51:27 +0200

    export StompContext

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import useSubscription from './hooks/useSubscription';
 import useStompClient from './hooks/useStompClient';
 import withStompClient from './hoc/withStompClient';
 import withSubscription from './hoc/withSubscription';
+import StompContext from './context/StompContext';
 
 export {
+  StompContext,
   StompSessionProvider,
   useSubscription,
   useStompClient,
